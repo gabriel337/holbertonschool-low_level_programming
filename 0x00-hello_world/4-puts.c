727bbdf58b69b42a19543dc71ed3d0728fa4f717
gabriel337 <g.david.lopezruiz@gmail.com>
2021-09-30 20:52:54 +0000

    prints a string with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main - prints a string
+ * Return: returns 0
+ */
 
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
+
 	return (0);
 }
