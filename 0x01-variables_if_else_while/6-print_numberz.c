4aff45a15fe93b1c59d72b816c8737055ace4a7d
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-04 13:54:03 +0000

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - will return if the number is - + or == to n
+ * Return: returns 0
+*/
+
+int main(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
