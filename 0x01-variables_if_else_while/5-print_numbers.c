1237587bb13c0b81fc605e37f1a2d14155da89a7
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-04 13:38:25 +0000

    prints all single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,28 +4,13 @@
  * main - will return if the number is - + or == to n
  * Return: returns 0
 */
-int printDigits(int n)
-{
-int digit;
-  
-if(n < 10)
-{
-digit = n;
-printf("%d\n", digit);
-}
-
-else
-{
-digit = printDigits(n/10);
-digit = printDigits(n%10);
-}
-return (digit);
-}
-
 int main(void)
 {
-int num = 3467678;
-printDigits(num);
-
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+}
+putchar('\n');
 return (0);
 }
