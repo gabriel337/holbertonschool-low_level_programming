f0720a683b9a8d0218948ffe8fd2c9ed71be41a4
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-05 19:10:08 +0000

    checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ */
+
+int _islower(int c)
+{
+	int r;
+
+	if (r == c)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_puchar('0');
+	}
+	return (0);
+}
