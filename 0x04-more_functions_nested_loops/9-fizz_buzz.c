c589eebeed6990b51cfeea2d1b36ee3fd249d242
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-08 02:15:53 +0000

    added task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints if mul of 3 Fizz if mul of 5 Buzz if mul of 15 FizzBuzz
+ * Return: returns int
+ */
+int main(void)
+{
+int i;
+
+for (i = 1; i < 100; i++)
+{
+if (i % 15 == 0)
+{
+printf("FizzBuzz \t");
+}
+else if ((i % 3) == 0)
+{
+printf("Fizz \t");
+}
+else if ((i % 5) == 0)
+{
+printf("Buzz \t");
+}
+else
+{
+printf("%d \t", i);
+}
+}
+return (0);
+}
