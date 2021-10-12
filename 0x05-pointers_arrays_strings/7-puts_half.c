4c374e6effb7eaad151a48fe0c8c561a60541d78
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-12 20:20:26 +0000

    added put half function

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ *@str: pointer
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+		str++;
+	i = i / 2;
+
+	for (i = i; i != '\0'; i--)
+		_putchar(*(str - i));
+	_putchar('\n');
+}
