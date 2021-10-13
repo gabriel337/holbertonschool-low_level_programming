d3d1e53604f9f0b66b2a24361c100032686823bb
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-13 21:40:12 +0000

    added task string that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strncpy - copies a string
+ * @dest: char destination
+ * @src: char source
+ * @n: number of byte
+ * Return: returns dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
+	return (dest);
 }
