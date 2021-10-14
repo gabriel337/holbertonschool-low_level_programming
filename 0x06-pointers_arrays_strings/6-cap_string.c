1b290f28a2263063248bfb999a4aa27f87fa3234
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-14 18:01:12 +0000

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,44 +8,30 @@
 char *cap_string(char *str)
 
 {
-	int i;
+	int i, j;
+	char array[] = {'\n', '\t', ' ', ',', ';', '.', '!',
+		'?', '"', '(', ')', '{', '}'};
+	int flag = 1;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0)
+		if (flag == 1)
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
-			continue;
-		}
-
-		if (str[i] == ' ')
-		{
-			++i;
-
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] = str[i] - 32;
-				continue;
 			}
+
+			flag = 0;
 		}
 
-		else
+		for (j = 0; array[j] != '\0'; j++)
 		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
+			if (str[i] == array[j])
+			{
+				flag = 1;
+			}
 		}
 	}
 	return (str);
 }
-
-
-
-
-
-
-
-
-
-
-
