838f0845cc2cf189ce36fe1aebc2f39eee5e6f6a
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-14 15:05:17 +0000

    added task func that reverses the content of an int array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of an array of integers
+ * @a: array
+ * @n: elements of the array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+
+		a[i] = a[n - i - 1];
+
+		a[n - i - 1] = temp;
+	}
+}
+
+
