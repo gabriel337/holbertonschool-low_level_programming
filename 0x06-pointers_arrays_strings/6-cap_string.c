b1730baa973ab9a4a424dde960722808db5807d9
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-14 17:41:26 +0000

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,39 @@
 #include "main.h"
 
-/*
- *
- *
+/**
+ * cap_string - capitalize first letter of every string
+ * @str: string being passed
+ * Return: returns str
  */
 char *cap_string(char *str)
 
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32; 
+				str[i] = str[i] - 32;
 			continue;
 		}
-		
-		if(str[i] == ' ')  
+
+		if (str[i] == ' ')
 		{
 			++i;
-			
-			if(str[i]>='a' && str[i]<='z')
+
+			if (str[i] >= 'a' && str[i] <= 'z')
 			{
-				str[i]=str[i]-32;
+				str[i] = str[i] - 32;
 				continue;
 			}
 		}
-		
+
 		else
 		{
-			if(str[i]>='A' && str[i]<='Z')
-				str[i]=str[i]+32;
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
 		}
 	}
 	return (str);
