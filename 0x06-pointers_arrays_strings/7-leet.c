f187828a48f3b762c47758fc5a8520d0d0607264
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-14 19:31:47 +0000

    added task swap letter with numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * leet - swaps letters[] with numbers []
+ * @str: string being passed
+ * Return: returns str
  */
 
 char *leet(char *str)
 {
 
-int i;
+int i, j;
+char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+char numbers[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 for (i = 0; str[i] != '\0'; i++)
-
-if(str[i] == 'a' || str[i] == 'A')
 {
-str[i] = 4;
-
+	for (j = 0; numbers[j] != '\0'; j++)
+	{
+		if (str[i] == letters[j])
+		{
+			str[i] = numbers[j];
+		}
+	}
 }
 return (str);
 }
