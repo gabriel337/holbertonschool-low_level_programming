28d35379a4fa1c356427a2636660bda4639c2516
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-22 16:12:37 +0000

    added new file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints commands
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
