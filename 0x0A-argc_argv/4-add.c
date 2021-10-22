6d00e1ef016ae2c1b7981caa45c3d54bf4b5bea0
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-22 22:54:28 +0000

    added new file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints argc added
+ * @argc: argument count
+ * @argv: argument array
+ * Return: returns 1 if argc is 1,
+ * if other than number are added returns 1
+ * returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 1)
+	{
+		printf("0");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
