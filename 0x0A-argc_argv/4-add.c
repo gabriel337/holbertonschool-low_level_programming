e4bf83083597126a4dbb74077a47eaa59252c75f
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-22 23:05:16 +0000

    changed code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,9 +11,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
 
-	if (argc < 1)
+int i, j, sum = 0;
+
+	if (argc < 2)
 	{
 		printf("0\n");
 		return (0);
@@ -21,10 +22,13 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
