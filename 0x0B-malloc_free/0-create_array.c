332b79d4c0a6a6861510f283abc3dfad267a69fd
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-26 14:29:10 +0000

    fixed error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,15 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *arr;
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
 	arr = malloc(size * sizeof(char));
 
-	if (arr == NULL && size == 0)
+	if (arr == NULL)
 	{
 		return (NULL);
 	}
