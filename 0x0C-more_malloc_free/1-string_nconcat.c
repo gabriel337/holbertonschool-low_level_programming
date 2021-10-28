928230656355487ec6247a304c8652d4a0292537
gabriel337 <g.david.lopezruiz@gmail.com>
2021-10-28 18:06:30 +0000

    concatenates 2 string with malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j = 0;
 	char *ptr;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		return ("");
-	}
+	if (s1 == NULL)
+		s1 = "";
+	
+	if (s2 == NULL)
+		s2 = "";
+	
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
