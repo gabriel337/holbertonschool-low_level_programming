9d462bc1d804fd0afab7a95fbb885ebd85f64142
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-01 19:24:16 +0000

    structure dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - h
+ * @name: name of dog
+ * @age:age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /*dog*/
