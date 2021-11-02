4061393616d587732e175b04e0ad8889ede2c206
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-02 00:32:43 +0000

    changed struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,17 @@
  * @age:age of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
-char *name;
-float age;
-char *owner;
-};
+  char *name;
+  float age;
+  char *owner;
+} dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+
 #endif /*dog*/
