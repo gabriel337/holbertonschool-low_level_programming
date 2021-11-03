3331c8dc146d10def2b4379f3f36fdbb19d85f08
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-03 17:39:52 +0000

    added task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function given as a parameter on each element of an array
+ * @array: array
+ * @size: size of array
+ * @action:pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
+
