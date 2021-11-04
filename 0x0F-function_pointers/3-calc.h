8c4b99b332ba6267db3b4b0323127a8dc101aa12
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-04 03:34:26 +0000

    fixed error

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
 /**
  * struct op - Struct op
  *
@@ -15,11 +23,4 @@ char *op;
 int (*f)(int a, int b);
 } op_t;
 
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 #endif /*CALC_H*/
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,8 +8,9 @@
  */
 int main(int argc, char **argv)
 {
-int number1, number2;
+int number1, number2, result;
 int (*calc)(int, int);
+char *get_op;
 
 	if (argc != 4)
 	{
@@ -17,11 +18,26 @@ int (*calc)(int, int);
 		exit(98);
 	}
 
-	calc = get_op_func(argv[2]);
-
+	get_op = argv[2];
 	number1 = atoi(argv[1]);
 	number2 = atoi(argv[3]);
 
-	printf("%d\n", calc(number1, number2));
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	{
+
+		printf("Error\n");
+		exit(100);
+	}
+
+	calc = get_op_func(get_op);
+	result = calc(number1, number2);
+
+	printf("%d\n", result);
 	return (0);
 }
