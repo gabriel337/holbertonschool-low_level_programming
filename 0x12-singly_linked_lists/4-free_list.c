9a19c337d898c83ca44c60835c7d27981e86b405
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-13 01:05:51 +0000

    added new task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - free memory from list
+ * @head: head of list
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
