37736c9ffdf21ab5316b1b4fd8291b67b068ab9e
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-16 17:46:37 +0000

    fixed betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
-
-	while (*head != NULL)
+	
+	if(head)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		while (*head != NULL)
+		{
+			tmp = *head;
+			*head = (*head)->next;
+			free(tmp);
+		}
 	}
-
 	else
 	{
 		return;
