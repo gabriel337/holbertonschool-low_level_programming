8b1ef3b79f52dc825b6c112eb94f9de729eb76cc
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-16 17:41:41 +0000

    added task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+ /**
+  * free_listint2 - frees list
+  * @head: head of list
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+}
