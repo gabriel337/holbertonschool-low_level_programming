3e86310fc6f6de03501d271082268c06b598d387
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-16 17:29:45 +0000

    added task

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,9 +9,9 @@ void free_listint(listint_t *head)
 
 	while (head != NULL)
 	{
-		tmp = head->next;
-		free(head->n);
-		free(head);
-		head = tmp;
+		tmp = head;
+		head = head->next;
+		free(tmp);
 	}
+	free(head);
 }
