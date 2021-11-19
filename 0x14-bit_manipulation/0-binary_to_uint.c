72dbf17561ac0b8ac98c5e2dc9d351915026f571
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-19 16:18:21 +0000

    added edge case

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,12 @@ unsigned int binary_to_uint(const char *b)
 
 	len = strlen(b);
 
+	if (b == NULL)
+		return (0);
+
 	for (c = len - 1; c >= 0; c--)
 	{
 		if (b[c] != '0' && b[c] != '1')
-			return (0);
-
-		if (b == NULL)
 			return (0);
 
 		if (b[c] == '1')
