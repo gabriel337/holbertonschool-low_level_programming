8030773e5abeb161c97bc9883a74a4a60f84edfd
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-20 00:20:00 +0000

    fixed code

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int bits;
-
-	bits = (n >> index) & 1;
-
-	if ((bits == 1) || (bits == 0))
-		return (bits);
-	else
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
+
+	n = n >> index;
+	n = n & 1;
+	return (n);
 }
