38314204518d78045af17fbb448d7927a1165103
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-26 17:01:40 +0000

    fixed a bug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,13 +8,14 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i, count = 0;
+	int fd, i;
+	size_t count = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 
 	fd = open(filename, O_APPEND | O_WRONLY, 00664);
 
