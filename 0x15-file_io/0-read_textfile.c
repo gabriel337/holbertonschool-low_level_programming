f47f7399788e3fdbee20051cb7ccb2b8134b8d5b
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-26 15:42:23 +0000

    added task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output
+ * @filename: pointer to string
+ * @letters: counts letters of string
+ * Return: number of letters it could read
+ * and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, read;
+	char *tmp = malloc(sizeof(char *) * letters);
+
+	if (tmp == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	read = read(fd, tmp, letters);
+	write(STDOUT_FILENO, tmp, read);
+
+	free(tmp);
+	close(fd);
+	return (read);
+}
