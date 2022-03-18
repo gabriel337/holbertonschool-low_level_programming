8dbca175a9d9b0cf406ca5c51e7447d05c82af3e
gabriel337 <g.david.lopezruiz@gmail.com>
2022-03-18 01:02:04 +0000

    fix typos:

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Moduel 5-island_perimeter
+Module 5-island_perimeter
 """
 
 
@@ -9,14 +9,18 @@ def island_perimeter(grid):
     row_num = len(grid)
     if row_num == 0 or len(grid[0]) == 0:
         return 0
+
     islands, overlaps = 0, 0
     col_num = len(grid[0])
+
     for i in range(row_num):
         for j in range(col_num):
             if (grid[i][j] == 1):
                 islands += 1
+
                 if (i < row_num - 1 and grid[i + 1][j] == 1):
                     overlaps += 1
+
                 if (j < col_num - 1 and grid[i][j + 1] == 1):
                     overlaps += 1
     return islands * 4 - overlaps * 2
